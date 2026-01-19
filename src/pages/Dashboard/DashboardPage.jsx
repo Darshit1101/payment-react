7397f9362c82072d9598d64c054b205a1c5bd024
Darshit Kakadiya <darshit.kakadiya@shunyavkash.com>
2026-01-19 16:19:17 +0530

    feat: refactor payment handling by moving PaymentModal to StripePage and simplifying DashboardPage

--- src/pages/Dashboard/DashboardPage.jsx
+++ src/pages/Dashboard/DashboardPage.jsx
@@ -1,42 +1,7 @@
-import axiosInstance from "../../api/axiosInstance";
-import { stripePromise } from "../../configs/stripe";
-import PaymentModal from "./PaymentModal";
-import Button from "@mui/material/Button";
-import { Elements } from "@stripe/react-stripe-js";
-import React, { useState } from "react";
+import StripePage from "../Stripe/StripePage";
 
 const DashboardPage = () => {
-  const [clientSecret, setClientSecret] = useState(null);
-
-  const createPaymentIntent = async () => {
-    try {
-      const res = await axiosInstance.post("/stripe/create-intent");
-      setClientSecret(res.clientSecret);
-    } catch (error) {
-      console.error("Error creating payment intent:", error);
-    }
-  };
-
-  if (!clientSecret) {
-    return (
-      <div style={{ margin: "20px" }}>
-        <h2>Payment Setup</h2>
-        <Button
-          variant="contained"
-          onClick={createPaymentIntent}
-          sx={{ mt: 2 }}
-        >
-          Pay Now
-        </Button>
-      </div>
-    );
-  }
-
-  return (
-    <Elements options={{ clientSecret }} stripe={stripePromise}>
-      <PaymentModal />
-    </Elements>
-  );
+  return <StripePage />;
 };
 
 export default DashboardPage;
---
+++ src/pages/Stripe/StripePage.jsx
@@ -0,0 +1,42 @@
+import axiosInstance from "../../api/axiosInstance";
+import { stripePromise } from "../../configs/stripe";
+import PaymentModal from "./PaymentModal";
+import Button from "@mui/material/Button";
+import { Elements } from "@stripe/react-stripe-js";
+import React, { useState } from "react";
+
+const StripePage = () => {
+  const [clientSecret, setClientSecret] = useState(null);
+
+  const createPaymentIntent = async () => {
+    try {
+      const res = await axiosInstance.post("/stripe/create-intent");
+      setClientSecret(res.clientSecret);
+    } catch (error) {
+      console.error("Error creating payment intent:", error);
+    }
+  };
+
+  if (!clientSecret) {
+    return (
+      <div style={{ margin: "20px" }}>
+        <h2>Payment Setup</h2>
+        <Button
+          variant="contained"
+          onClick={createPaymentIntent}
+          sx={{ mt: 2 }}
+        >
+          Pay Now
+        </Button>
+      </div>
+    );
+  }
+
+  return (
+    <Elements options={{ clientSecret }} stripe={stripePromise}>
+      <PaymentModal />
+    </Elements>
+  );
+};
+
+export default StripePage;
