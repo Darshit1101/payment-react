903ca7d982cb4810c25001878a4eaa87d0b8a0f5
Darshit Kakadiya <darshit.kakadiya@shunyavkash.com>
2026-01-19 15:52:41 +0530

    feat: update axios instance configuration with base URL and remove unnecessary headers

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,17 +1,14 @@
 import { VITE_BACKEND_URL } from "../constants/environment.js";
 import axios from "axios";
 
+const appName = "payment";
+const appVersion = "v1";
+
 const axiosInstance = axios.create({
-  baseURL: VITE_BACKEND_URL,
+  baseURL: VITE_BACKEND_URL + "/" + appName + "/" + appVersion,
   timeout: 10000,
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
 
-/* ======================
-   REQUEST INTERCEPTOR
-====================== */
 axiosInstance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
@@ -25,16 +22,12 @@ axiosInstance.interceptors.request.use(
   (error) => Promise.reject(error),
 );
 
-/* ======================
-   RESPONSE INTERCEPTOR
-====================== */
 axiosInstance.interceptors.response.use(
   (response) => response.data,
   (error) => {
     if (error.response) {
       console.error("API Error:", error.response.data);
 
-      // Example: auto logout on 401
       if (error.response.status === 401) {
         localStorage.removeItem("token");
         window.location.href = "/login";
